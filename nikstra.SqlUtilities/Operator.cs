f661f8ba1e2f86dd5c5215a996b451e498767477
Niklas Strand <nikstra@users.noreply.github.com>
2020-06-08 22:32:10 +0200

    Add Operator class.

---
+++ nikstra.SqlUtilities/Operator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nikstra.SqlUtilities
+{
+    public class Operator
+    {
+        public string Value { get; }
+        private Operator(string @operator) =>
+            Value = @operator;
+
+        public static Operator Equal { get; } = new Operator("=");
+        public static Operator GreaterThan { get; } = new Operator(">");
+        public static Operator GreaterThanOrEqual { get; } = new Operator(">=");
+        public static Operator LessThan { get; } = new Operator("<");
+        public static Operator LessThanOrEqual { get; } = new Operator("<=");
+        public static Operator NotEqual { get; } = new Operator("!=");
+
+        public override string ToString() =>
+            Value;
+
+        public static implicit operator string(Operator @operator) =>
+            @operator.Value;
+    }
+}
